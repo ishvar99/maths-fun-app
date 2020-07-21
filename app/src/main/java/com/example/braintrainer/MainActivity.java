c3bf6281e332aa0d8b98206caa397089b6c305ee
ishvar99 <ishanvarshney99@gmail.com>
2020-07-21 19:07:05 +0530

    added reset game functionality and improved logic

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -16,24 +16,50 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayout linearLayout;
-    Button button,timerButton;
+    Button button,timerButton,scoreButton,playAgain;
+    Button[] option=new Button[4];
+    int[] option_ids={R.id.option1,R.id.option2,R.id.option3,R.id.option4};
     CountDownTimer timer;
     TextView textView;
     int totalTime=25000;
     int timeInterval=1000;
     int counter=0;
-    String[] questions={"(100-37)+89","(133+21)/7)"};
+    int score=0;
+    String[] questions={"100-37+89","133+21/7"};
     Integer[] answers={152,22};
-    public String fetchQuestion(int i){
-        return questions[i];
+    Integer[][] options={{152,157,145,160},{136,22,150,35}};
+    public void playAgain(View view){
+        counter=0;
+        score=0;
+        startGame(view);
+    }
+    public void setQuestion(int i){
+        Log.i("counter",String.valueOf(counter));
+        textView.setText(questions[i]);
+        for(int k=0;k<4;k++){
+            option[k]=findViewById(option_ids[k]);
+            option[k].setText(String.valueOf(options[i][k]));
+        }
+        counter++;
     }
     public void submitAnswer(View view){
-        Log.i("info",view.toString());
-        button=(Button)view;
-        Log.i("tag",button.getTag().toString());
+        checkAnswer((Button) view);
     }
-
-    public void startTimer(){
+    public void updateScore(){
+        score++;
+        scoreButton.setText(score+"/5");
+    }
+    public void checkAnswer(Button btn){
+        if(btn.getText().toString().equals("152")){
+            updateScore();
+            setQuestion(counter);
+        }
+        else{
+            setQuestion(counter);
+        }
+    }
+    public void startTimer()
+    {
         timer = new CountDownTimer(totalTime+100,timeInterval) {
             @Override
             public void onTick(long l) {
@@ -41,23 +67,24 @@ public class MainActivity extends AppCompatActivity {
             }
             @Override
             public void onFinish() {
-                Log.i("INFO","TIMES UP!");
+                playAgain.setVisibility(View.VISIBLE);
             }
         }.start();
     }
-    public void checkResult(){
-
-    }
 
     public void startGame(View view){
+        playAgain=findViewById(R.id.playAgain);
+        playAgain.setVisibility(View.INVISIBLE);
         button =findViewById(R.id.button);
+        scoreButton=findViewById(R.id.scoreButton);
         textView=findViewById(R.id.questionText);
         linearLayout=findViewById(R.id.linearLayoutMain);
         linearLayout.setVisibility(View.VISIBLE);
         button.setVisibility(View.INVISIBLE);
         timerButton=findViewById(R.id.timerButton);
-        textView.setText(fetchQuestion(counter));
         startTimer();
+        scoreButton.setText(score+"/5");
+        setQuestion(counter);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
